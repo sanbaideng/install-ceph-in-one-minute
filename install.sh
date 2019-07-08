fc260bc3d3cb84a8d74f6824572e94d3fef4b216
YangYang <405102554@qq.com>
2019-07-08 16:11:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,24 @@ gpgcheck=0
 type=rpm-md
 gpgkey=https://mirrors.163.com/ceph/keys/release.asc
 priority=1
+
+[Ceph-noarch]
+name=Ceph noarch packages
+baseurl=http://mirrors.163.com/ceph/rpm-jewel/el7/noarch
+enabled=1
+gpgcheck=0
+type=rpm-md
+gpgkey=https://mirrors.163.com/ceph/keys/release.asc
+priority=1
+
+[ceph-source]
+name=Ceph source packages
+baseurl=http://mirrors.163.com/ceph/rpm-jewel/el7/SRPMS
+enabled=1
+gpgcheck=0
+type=rpm-md
+gpgkey=https://mirrors.163.com/ceph/keys/release.asc
+priority=1
 " > /etc/yum.repos.d/ceph.repo
 
 yum install ceph ceph-radosgw ceph-deploy -y
